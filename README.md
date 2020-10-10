0a36c755256a4d8b654d11f71dc6a7a8ae18bfd4
Jasom Park <zeipar@gmail.com>
2020-10-10 16:43:33 -0700

    Updated useTabs hook. Updated readme to show current progress.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-hooks
+
 Created react hooks to be deployed in npm so other users can use them.
+
+- [x] useInput
+- [x] useTabs
+- [x] useTitle
--- useTabs/useTabs.js
+++ useTabs/useTabs.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 export const useTabs = (initialTab, allTabs) => {
   const [currentIndex, setCurrentIndex] = useState(initialTab);
   if (!allTabs || !Array.isArray(allTabs)) {
