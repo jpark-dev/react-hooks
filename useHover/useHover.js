6392303b3d293581f9d8a37d88ee8b5c30e2acfa
Jasom Park <zeipar@gmail.com>
2020-10-10 17:14:02 -0700

    Created useHover hook that binds an event when mouse cursor hovers. Very similar to useClick.

---
+++ useHover/useHover.js
@@ -0,0 +1,17 @@
+const useClick = onHover => {
+  if (typeof onClick !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("mouseeneter", onHover);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("mouseenter", onHover);
+      }
+    };
+  }, []);
+  return element;
+};
